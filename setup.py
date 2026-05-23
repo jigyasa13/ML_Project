36a82a7b913d72f1d7ad52d6f301053f9bc6e2b9
jigyasa.13 <jigya.1223@gmail.com>
2026-05-24 01:37:37 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+#Responsible to create my ML application as package
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+def get_requirements(file_path:str)->List[str]:
+ #this function will return the list of requirements
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","  ") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+     
+    return requirements
+
+
+
+setup(
+name="ML_Project",
+version="0.0.1",
+author="Jigyasa Bharti",
+author_email="jigya.1223@gmail.com",
+packages=find_packages(),  #sees how many folders have _init.py file and consider it as package
+install_requires=get_requirements("requirements.txt")  #take all packages from requirements.txt
+
+)
